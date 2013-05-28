d0cf7762af63705a3cfc14fe2621544923447034
ih <irvin.hwang@gmail.com>
2013-05-28 08:58:02 -0700

    using router for viewing expanders

---
+++ client/router.js
@@ -0,0 +1,8 @@
+Meteor.Router.add ({
+    '/':  'welcome', 
+    '/expanders/:_id':  {
+	to:  'expanderViewer', 
+	    // TODO rename key to id
+	and:  function (id) { Session.set ('selectedExpanderKey',  id); }
+    }
+});
