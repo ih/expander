a19372f48cf58669fc0a2ce694f4ef6c2d88c3c6
ih <irvin.hwang@gmail.com>
2013-06-02 12:02:24 -0700

    changed expander creation to be server side

--- client/creator.js
+++ client/creator.js
@@ -4,16 +4,19 @@ Template.expanderCreator.events({
         var self = this;
         var newContent = template.find('textarea').value;
         //create a new expander
-        var newExpanderId = Expanders.insert({
-            //TODO rename parentId
-            parent : self.parent._id,
+	// server-side createExpander will fill in fragment's id attribute
+	var fragment = {border : self.border, id : undefined};
+	var newExpanderData = {
+	    parent : self.parent._id,
             content : newContent,
-            parentFragment : self.selectionString,
-            fragments : []
-        });
-        //add fragment information to current expander
-        var fragment = {border : self.border, id : newExpanderId};
-        Expanders.update({_id : self.parent._id},
-                         { $push: { fragments : fragment }});
+            parentFragment : self.selectionString
+	};
+	Meteor.call ('createExpander', {newExpanderData:  newExpanderData,  fragment: fragment}, 
+		     function (error, id) {
+			 if (error) {
+			     return alert (error.reason);
+			 }
+			 return true;
+		     });
     }
 });
--- collections/expanders.js
+++ collections/expanders.js
@@ -1,2 +1,26 @@
 // declared without var so accessible outside this file
 Expanders = new Meteor.Collection('expanders');
+
+Meteor.methods ({
+    createExpander:  function (dataFromClient) {
+	var user = Meteor.user ();
+	if (!user) {
+	    throw new Meteor.Error (401,  'You need to log in to create a new expander');
+	}
+	    // pick out the whitelisted keys
+	    // TODO rename parent to parentId
+	var expanderData = _.pick (dataFromClient.newExpanderData,
+				   'parent', 'content', 'parentFragment');
+	    // add additional data to the new expander
+	var additionalExpanderData = {
+	    creatorId: user._id,
+	    creationDate: new Date ().getTime (),
+	    fragments: []
+	};
+	_.extend (expanderData, additionalExpanderData);
+	var newExpanderId = Expanders.insert (expanderData);
+	var fragment = dataFromClient.fragment;
+	fragment.id = newExpanderId;
+	Expanders.update (expanderData.parent, {$push: {fragments: fragment}});
+    }
+});
