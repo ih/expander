153bfc64ec986717d8cee00089bd85f088ba0ffc
ih <irvin.hwang@gmail.com>
2013-07-06 09:16:13 -0700

    changed layout to two column, added an editor template

--- client/router.js
+++ client/router.js
@@ -1,6 +1,8 @@
 Meteor.Router.add ({
     '/':  'welcome',
     '/expanders/:_id':  {
+		// TODO change to use expander template directly and change template
+		// so that it takes an expander key
 	to:  'expanderViewer',
 	    // TODO rename key to id
 	and:  function (id) { Session.set ('selectedExpanderKey',  id); }
