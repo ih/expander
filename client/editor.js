fa42bb49ae710ffa098e276e412642150cd7bb3c
ih <irvin.hwang@gmail.com>
2013-07-24 00:05:35 -0700

    more development on adjusting fragment boundaries when editing, should be simplified

--- client/editor.js
+++ client/editor.js
@@ -1,3 +1,11 @@
+// move all fragment boundaries after the caretPosition forward or backward 
+var adjustFragmentBoundaries = 
+	function(lengthDelta, caretPosition, expander) {
+		_.each(expander.fragments, function(fragment) {
+			console.log(fragment.border);
+		});
+	};
+
 Template.expanderEditor.helpers ({
 	editingExpander: function () {
 		return Session.get('editingExpander');
@@ -9,33 +17,51 @@ Template.expanderEditor.preserve(['.editor textarea']);
 
 Template.expanderEditor.events({
 	'input .editor textarea': function(event, template) {
-		console.log('input!');
+		var self = this;
+		// every time a change to the editor happens trigger an
+		// insertion or deletion event
 		var newContent = event.target.value;
+		var oldContent = Session.getObjectValue('editingExpander', 'content');
+		// insertion also in the equal length case since deleting text always
+		// decreases the length
+		var lengthDelta = newContent.length - oldContent.length;
+		var caretPosition = $('.editor textarea').caret();
+		if(lengthDelta >= 0) {
+			// insertion event so increase the fragment boundaries after
+			// the caret
+			console.log('insertion');
+			adjustFragmentBoundaries(lengthDelta, caretPosition, self);
+		}
+		else {
+			// deletion event so decrease the fragment boundaries after
+			// the caret
+			console.log('deletion');
+		}
 		Session.setObjectValue('editingExpander', 'content', newContent);
-
 	}
 });
+
+
 
 /*
  get
 */
-function getTextChangeEventData() {
 
-}
+// http://stackoverflow.com/questions/14153205/->
+// how-to-trigger-a-custom-event-with-meteor-js
 
-// http://stackoverflow.com/questions/14153205/how-to-trigger-a-custom-event-with-meteor-js
-	/* 
-function attachEvents () {
-	var editorSelector = '.editor textarea';
-	$(editorSelector).on('insertion', function() {
-		console.log('insertion');
-	});
-	$(editorSelector).on('deletion', function() {
-		console.log('deletion');
-	});
+	// function attachEvents () {
+	// var editorSelector = '.editor textarea';
+	// $(editorSelector).on('insertion', function() {
+	// 	console.log('insertion');
+	// });
+	// $(editorSelector).on('deletion', function() {
+	// 	console.log('deletion');
+	// });
 
 	// every time a change to the editor happens trigger an
 	// insertion or deletion event
+		/* 
 	$(editorSelector).bind('input propertychange', function (eventObject) {
 		var eventData = getTextChangeEventData();
 		var rando = choose([1, 2]);
@@ -48,12 +74,12 @@ function attachEvents () {
 		var newContent = eventObject.target.value;
 		Session.setObjectValue('editingExpander', 'content', newContent);
 	});
-
+		 */
 	
-}
+// }
 
-Template.expanderEditor.rendered = function (){
-		attachEvents ();
-};
+// Template.expanderEditor.rendered = function (){
+// 	attachEvents ();
+// };
 
-*/
+
