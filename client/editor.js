f7ee97e213b96018bf96bc505f22f8f037ca100a
ih <irvin.hwang@gmail.com>
2013-07-26 13:15:30 -0700

    editor moves fragment boundaries as text is added/removed

--- client/editor.js
+++ client/editor.js
@@ -2,8 +2,13 @@
 var adjustFragmentBoundaries = 
 	function(lengthDelta, caretPosition, expander) {
 		_.each(expander.fragments, function(fragment) {
-			console.log(fragment.border);
+			// only need to adjust fragments after the point of edit
+			if(fragment.border.open > caretPosition) {
+				fragment.border.open += lengthDelta;
+				fragment.border.close += lengthDelta;
+			}
 		});
+		return expander.fragments;
 	};
 
 Template.expanderEditor.helpers ({
@@ -17,69 +22,26 @@ Template.expanderEditor.preserve(['.editor textarea']);
 
 Template.expanderEditor.events({
 	'input .editor textarea': function(event, template) {
+		/*
+		 * Compare the length before and after a change, if the content grew
+		 * then text was inserted so increase the fragment borders after
+		 * the point of insertion, do the opposite if the content shrank i.e.
+		 * some text was deleted
+		 */
 		var self = this;
-		// every time a change to the editor happens trigger an
-		// insertion or deletion event
 		var newContent = event.target.value;
 		var oldContent = Session.getObjectValue('editingExpander', 'content');
 		// insertion also in the equal length case since deleting text always
 		// decreases the length
 		var lengthDelta = newContent.length - oldContent.length;
 		var caretPosition = $('.editor textarea').caret();
-		if(lengthDelta >= 0) {
-			// insertion event so increase the fragment boundaries after
-			// the caret
-			console.log('insertion');
-			adjustFragmentBoundaries(lengthDelta, caretPosition, self);
-		}
-		else {
-			// deletion event so decrease the fragment boundaries after
-			// the caret
-			console.log('deletion');
-		}
+		// TODO update expander properties all at once if 
+		// efficiency an issue
+		var newFragments =
+				adjustFragmentBoundaries(lengthDelta, caretPosition, self);
+		Session.setObjectValue('editingExpander', 'fragments', newFragments);
 		Session.setObjectValue('editingExpander', 'content', newContent);
 	}
 });
-
-
-
-/*
- get
-*/
-
-// http://stackoverflow.com/questions/14153205/->
-// how-to-trigger-a-custom-event-with-meteor-js
-
-	// function attachEvents () {
-	// var editorSelector = '.editor textarea';
-	// $(editorSelector).on('insertion', function() {
-	// 	console.log('insertion');
-	// });
-	// $(editorSelector).on('deletion', function() {
-	// 	console.log('deletion');
-	// });
-
-	// every time a change to the editor happens trigger an
-	// insertion or deletion event
-		/* 
-	$(editorSelector).bind('input propertychange', function (eventObject) {
-		var eventData = getTextChangeEventData();
-		var rando = choose([1, 2]);
-		if(rando === 1) {
-			$(editorSelector).trigger('insertion');
-		}
-		else {
-			$(editorSelector).trigger('deletion');
-		}
-		var newContent = eventObject.target.value;
-		Session.setObjectValue('editingExpander', 'content', newContent);
-	});
-		 */
-	
-// }
-
-// Template.expanderEditor.rendered = function (){
-// 	attachEvents ();
-// };
 
 
